76c07d5b8cf90c1281a70e8846bafbb576435b2d
arrowed <manny@arrowed.org>
2024-01-13 12:46:47 +1100

    Last widescreen image is hero, faux thumbnail others

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { io } from "socket.io-client";
 
 const WebSocketApp = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState('');
-  const [connection, setConnection] = useState(null);
+  const [focussed, setFocusImages] = useState([]);
+  const [other, setOtherImages] = useState([]);
+  const [connection, setConnection] = useState(null); //eslint-disable-line
   
-
   useEffect(() => {
     // WebSocket connection setup
     const wsConnection = io("ws://localhost:8080");
@@ -15,15 +14,14 @@ const WebSocketApp = () => {
       console.log('WebSocket connection opened');
     });
 
-      wsConnection.on("packet", ({ type, data }) => {
-        const newMessages = [...messages, type];
-        setMessages(newMessages);
-      });
-
     wsConnection.onAny((eventName, ...args) => {
-      const newMessages = [args[0], ...messages];
-      console.log(args);
-      setMessages(newMessages);
+      if (args[0]['image']['classification'] === 'widescreen') {
+        const updatedWidescreens = [args[0], ...focussed].slice(0, 50);
+        setFocusImages(updatedWidescreens);
+      } else {
+        const updatedOtherResolutions = [args[0], ...other].slice(0, 50);
+        setOtherImages(updatedOtherResolutions);
+      }
     });
 
     wsConnection.on("disconnect", () => {
@@ -36,36 +34,32 @@ const WebSocketApp = () => {
     return () => {
       wsConnection.close();
     };
-  }, [messages]);
+  }, [focussed, other]);
 
-  const sendMessage = () => {
-    if (connection && inputMessage.trim() !== '') {
-      connection.emit(inputMessage);
-      setInputMessage('');
-    }
-  };
+  const setHero = (image) => {
+      const updatedWidescreens = [image, ...focussed];
+      setFocusImages(updatedWidescreens);
+  }
 
   return (
     <div>
-      <h1>WebSocket React App</h1>
       <div>
-        <h2>Messages:</h2>
-        <ul>
-          {messages.map((message, index) => (
-            <li key={index}>{message['image']['data_path']}
-              <img alt={message['image']['data_path']}
-                src={"http://localhost:8080/image/" + message['image']['data_path']} />
-            </li>
-          ))}
-        </ul>
+          {focussed[0] && 
+            <img alt={ focussed[0]['image']['data_path']}
+              src={"http://localhost:8080/image/" + focussed[0]['image']['data_path']} 
+              height="80%"
+              />
+            }
       </div>
       <div>
-        <input
-          type="text"
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-        />
-        <button onClick={sendMessage}>Send Message</button>
+        {other.slice(0, 10).map((message, index) => (
+            <img alt={message['image']['data_path']}
+              src={"http://localhost:8080/image/" + message['image']['data_path']} 
+              width="10%"
+              height="10%" 
+              onClick={() => setHero(message)}
+              />
+        ))}
       </div>
     </div>
   );
